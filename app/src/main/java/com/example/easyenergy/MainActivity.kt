1615a3b516abe5225136ce28ed7cc78b3c85c56e
irnuska <100119022+irnuska@users.noreply.github.com>
2023-03-26 14:23:20 +0300

    Lisätty navigaatiot ja vaadittavia xml-tiedostoja edistyneen projektin mukaisesti. Myös manifestia muokattu jotta saadaan activity käyntiin. HOX! Tällä hetkellä ei osaa inflatoida oikein näkymää.

--- app/src/main/java/com/example/easyenergy/MainActivity.kt
+++ app/src/main/java/com/example/easyenergy/MainActivity.kt
@@ -2,20 +2,46 @@ package com.example.easyenergy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.easyenergy.databinding.ActivityMainBinding
-//import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    //TODO: vaatii muutaman gradle lisäyksen, lisäksi vaaditaan navigation
-    //private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolBar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.mainFragment, R.id.infoFragment, R.id.settingsFragment), drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/easyenergy/fragments/MainFragment.kt
+++ app/src/main/java/com/example/easyenergy/fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.easyenergy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,20 +13,26 @@ class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewModel: MainViewModel
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // Inflate the layout for this fragment
+        //TODO: ei osaa oikein inflatea fragmenttia
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        return binding.root
+
+        val root: View = binding.root
+
+        return root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 

b0a553ae34b947610a42a7080910676d0d0ff0c4
irnuska <100119022+irnuska@users.noreply.github.com>
2023-04-09 12:30:23 +0300

    Muokattu manifestia jotta saadaan nettiyhteys sekä mahdollistetaan http-liikenne. Kopioitu datareadfragmentin toiminnallisuus viewmodeliin ja muokattu siten että lisää listaan datan. Lisätty testinappi.

--- app/src/main/java/com/example/easyenergy/fragments/MainFragment.kt
+++ app/src/main/java/com/example/easyenergy/fragments/MainFragment.kt
@@ -53,46 +53,16 @@ class MainFragment : Fragment() {
             binding.aaChartView.aa_drawChartWithChartModel(viewModel.getChart)
         }
 
+        binding.buttonForTesting.setOnClickListener()
+        {
+            viewModel.getAllData(this.requireContext())
+        }
+
+
         return binding.root
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
-    fun getElectrictyData() {
-        val JSON_URL = "http://spotprices.energyecs.frostbit.fi/api/v1/prices"
-
-        // Request a string response from the provided URL.
-        val stringRequest: StringRequest = object : StringRequest(
-            Request.Method.GET, JSON_URL,
-            Response.Listener { response ->
-                // print the response as a whole
-                // we can use GSON to modify this response into something more usable
-                Log.d("ADVTECH", response)
-
-            },
-            Response.ErrorListener {
-                // typically this is a connection error
-                Log.d("ADVTECH", it.toString())
-            })
-        {
-            @Throws(AuthFailureError::class)
-            override fun getHeaders(): Map<String, String> {
-
-                // basic headers for the data
-                val headers = HashMap<String, String>()
-                headers["Accept"] = "application/json"
-                headers["Content-Type"] = "application/json; charset=utf-8"
-                return headers
-            }
-        }
-
-        // Add the request to the RequestQueue. This has to be done in both getting and sending new data.
-        // if using this in an activity, use "this" instead of "context"
-        val requestQueue = Volley.newRequestQueue(context)
-        requestQueue.add(stringRequest)
-    }
-
 }
--- app/src/main/java/com/example/easyenergy/fragments/MainViewModel.kt
+++ app/src/main/java/com/example/easyenergy/fragments/MainViewModel.kt
@@ -1,16 +1,27 @@
 package com.example.easyenergy.fragments
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.easyenergy.datatypes.ElectricityPrice
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel(){
     private val hoursList = mutableListOf<Double>()
-
-
+    private var allDataList = mutableListOf<ElectricityPrice>()
     private lateinit var testChart : AAChartModel
     val getChart get() = testChart
+    var chosenYear: String = "2022"
 
 
 
@@ -79,5 +90,51 @@ class MainViewModel : ViewModel(){
                     .data(hoursList.toTypedArray())
             )
             )
+    }
+
+    fun getAllData(context: Context) {
+        viewModelScope.launch {
+            val JSON_URL = "http://spotprices.energyecs.frostbit.fi/api/v1/prices"
+
+            val gson = GsonBuilder().setPrettyPrinting().create()
+            // Request a string response from the provided URL.
+            val stringRequest: StringRequest = object : StringRequest(
+                Request.Method.GET, JSON_URL,
+                Response.Listener { response ->
+                    var result : List<ElectricityPrice> = gson.fromJson(response, Array<ElectricityPrice>::class.java).toList()
+
+                    //lisää atm valitun vuoden datan listaan, kevyempi myös testata vuoden datalla
+                    for (item: ElectricityPrice in result)
+                    {
+                        val timeValSub = item.Time?.substring(0,4)
+                        if (timeValSub == chosenYear)
+                        {
+                            allDataList.add(item)
+                        }
+
+                    }
+                    Log.d("ADVTECH", allDataList.size.toString())
+
+                },
+                Response.ErrorListener {
+                    // typically this is a connection error
+                    Log.d("ADVTECH", it.toString())
+                }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+
+                    // basic headers for the data
+                    val headers = HashMap<String, String>()
+                    headers["Accept"] = "application/json"
+                    headers["Content-Type"] = "application/json; charset=utf-8"
+                    return headers
+                }
+            }
+
+            // Add the request to the RequestQueue. This has to be done in both getting and sending new data.
+            // if using this in an activity, use "this" instead of "context"
+            val requestQueue = Volley.newRequestQueue(context)
+            requestQueue.add(stringRequest)
+        }
     }
 }
